61af9583e84844896d50933b35eac3b1f82c7b1d
Hashim khan <hashimkhanlazak0301@gmail.com>
2025-11-09 23:24:14 +0500

    today i have solve problem the reverse method for string

--- linked.js
+++ linked.js
@@ -4,3 +4,20 @@ let con = Number(number);
 for(i=1; i<=10; i++){
     console.log(`${number}X${i} = ${number*i}`);
 }
+
+//today we practie on reverse a string.
+//1st method we used built in function
+let str = "javascript";
+let  reversed = str.split("").reverse().join("");
+console.log(reversed);
+
+//2nd method 
+let str2 = "javscript is powerfull";
+let reverse1 = "";
+
+for(let i=str2.length-1; i>=0; i--){
+    reverse1+=str2[i];
+}
+console.log("this is same and original:",str2);
+console.log(reverse1);
+
